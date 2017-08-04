762d145cdda8e8e45d43bc263164acf450f16fa7
IagoLast <iago.lastra@gmail.com>
2017-08-04 10:14:27 +0200

    Add custom ccss validator

---
+++ src/validator.js
@@ -0,0 +1,19 @@
+/**
+ * Validates ccss against custom defined rules.
+ */
+function validateCSS(ccss) {
+  _validateNotFillFile(ccss);
+}
+
+/**
+ * Throw an exception if the ccss has colored file makers.
+ * Tangram cannot handle painting a image causing a bad result.
+ */
+function _validateNotFillFile(ccss) {
+  if (ccss.includes('marker-fill') && ccss.includes('marker-file')) {
+    throw new Error('tangram don\'t support colored markers: marker-file and marker-fill properties are not compatible');
+  }
+}
+
+
+module.exports = { validateCSS }
