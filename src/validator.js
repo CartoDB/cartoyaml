ce51ccb618b7cfa29d94b1058af35d1852d447bb
IagoLast <iago.lastra@gmail.com>
2017-08-04 10:35:42 +0200

    Use indexOf instead includes

--- src/validator.js
+++ src/validator.js
@@ -10,7 +10,7 @@ function validateCSS(ccss) {
  * Tangram cannot handle painting a image causing a bad result.
  */
 function _validateNotFillFile(ccss) {
-  if (ccss.includes('marker-fill') && ccss.includes('marker-file')) {
+  if ((ccss.indexOf('marker-fill') >= 0) && (ccss.indexOf('marker-file') >= 0)) {
     throw new Error('tangram don\'t support colored markers: marker-file and marker-fill properties are not compatible');
   }
 }
